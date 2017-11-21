98d423f51c1a5b8676a558ad7d7f1dbdc4907852
William Elliott <wcelliott1991@gmail.com>
2017-11-21 09:46:59 -0500

    adds redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
+
 class App extends Component {
     render() {
         return (
